db6c247a9dac4f6c26726cf888e7cfea4dfb8e69
unknown <pasanpahasara7788@gmail.com>
2022-05-04 22:08:16 +0530

    Update Item Generate New Code

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -63,4 +63,16 @@ public class ItemDAOImpl {
         pstm.setString(1, code);
         return pstm.executeUpdate() > 0;
     }
+
+    public String generateNewCode() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        ResultSet rst = connection.createStatement().executeQuery("SELECT code FROM Item ORDER BY code DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("code");
+            int newItemId = Integer.parseInt(id.replace("I00-", "")) + 1;
+            return String.format("I00-%03d", newItemId);
+        } else {
+            return "I00-001";
+        }
+    }
 }
